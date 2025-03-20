ad984d23363093b5a9e66a16775d0ab146f92845
ErikHenderson-dev <erikhenderson.dev@gmail.com>
2025-03-20 20:25:02 -0300

    refact: adjust application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  # def authenticate_user!
-    # true
-    # redirect_to new_user_session_path unless user_signed_in?
-  # end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
